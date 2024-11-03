03eb1aa98162c488904ca99b87d1081245b6b6d6
Naveen3521 <87026171+Naveen3521@users.noreply.github.com>
2024-11-03 18:55:05 +0530

    Update TypeWiseAlert.cs

--- TypeWiseAlert.cs
+++ TypeWiseAlert.cs
@@ -6,6 +6,7 @@
       TOO_LOW,
       TOO_HIGH
     };
+    
     public static BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
       if(value < lowerLimit) {
         return BreachType.TOO_LOW;
